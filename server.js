cd3342b8df6a108e8881d0f9ec306729b4d2a88f
Jo Minji <mingi2687@gamil.com>
2023-05-01 10:57:50 +0900

    서버를 구성할 때 필요한 코드와 아닌 코드 구분

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 import express from "express";
 
+const html = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Todo List</title>
+  </head>
+  <body>
+  <div id="app">
+    <ul>
+      <li>SSR이 뭔데?</li>
+      <li>CSR은 뭔데?</li>
+      <li>이게 왜 필요한건데?</li>
+      <li>MVVM은 또 뭐야?</li>
+      <li>공부해야 할게 왜이렇게 많아?</li>
+    </ul>
+  </div>
+  </body>
+  </html>
+`;
+
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send(`
-         <!DOCTYPE html>
-            <html lang="en">
-            <head>
-            <meta charset="UTF-8">
-            <title>Todo List</title>
-            </head>
-            <body>
-            <div id="app">
-            <ul>
-                <li>SSR이 뭔데?</li>
-                <li>CSR은 뭔데?</li>
-                <li>이게 왜 필요한건데?</li>
-                <li>MVVM은 또 뭐야?</li>
-                <li>공부해야 할게 왜이렇게 많아?</li>
-            </ul>
-            </div>
-            </body>
-        </html>
-    `);
-});
-
+app.get("/", (req, res) => res.send(html));
 app.listen(8000, () => console.log("listen to http"));
